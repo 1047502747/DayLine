f067054f96dcc42494a6e03abcae13bd36f12a29
admin2 <630509237@qq.com>
2016-04-23 14:50:11 +0800

    一次性太多了

--- DayLine/DayLine/PostViewController.m
+++ DayLine/DayLine/PostViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "PostViewController.h"
-
+#import "dynamicTableViewCell.h"
 @interface PostViewController ()
+@property (strong, nonatomic) NSMutableArray *objectsForShow;
+@property (strong, nonatomic) UIActivityIndicatorView *aiv;
 
 @end
 
@@ -16,8 +18,78 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self refreshData];
+    _tableView.tableFooterView = [[UIView alloc] init];
+    UIRefreshControl *rc = [[UIRefreshControl alloc] init];
+    rc.tag = 10001;
+    rc.tintColor = [UIColor darkGrayColor];
+    [rc addTarget:self action:@selector(refreshData) forControlEvents:UIControlEventValueChanged];
+    [_tableView addSubview:rc];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(requestData) name:@"RefreshPost" object:nil];    // Do any additional setup after loading the view.
 }
+
+
+
+
+- (void)requestData {
+    _aiv = [Utilities getCoverOnView:self.view];
+    [self refreshData];
+}
+
+
+//数据请求
+- (void)refreshData {
+    [_objectsForShow removeAllObjects];
+    PFUser *currentUser = [PFUser currentUser];
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"poster != %@", currentUser];
+    PFQuery *query = [PFQuery queryWithClassName:@"Posts" predicate:predicate];
+    [query orderByDescending:@"updateAt"];
+    [query includeKey:@"poster"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
+        [_aiv stopAnimating];
+        UIRefreshControl *rc = (UIRefreshControl *)[_tableView viewWithTag:10001];
+        [rc endRefreshing];
+        if (!error) {
+            NSLog(@"objects = %@",objects);
+            _objectsForShow = [NSMutableArray arrayWithArray:objects];
+            [_tableView reloadData];
+            
+        }else{
+            NSLog(@"Error: %@",error.userInfo);
+            [Utilities popUpAlertViewWithMsg:@"请保持网络连接畅通" andTitle:nil onView:self];
+        }
+    }];
+    
+    //让导航条失去交互能力
+    self.navigationController.view.userInteractionEnabled = NO;
+    //在根视图上创建一朵菊花，并且让它转动
+    UIActivityIndicatorView *aiv = [Utilities getCoverOnView:self.view];
+    [aiv stopAnimating];
+    
+    
+    
+}
+
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _objectsForShow.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+   dynamicTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell2" forIndexPath:indexPath];
+    
+    PFObject *obj = _objectsForShow[indexPath.row];
+    PFUser *user = obj[@"poster"];
+    NSString *name = user[@"nickname"];
+    NSNumber *praise = obj[@"praise"];
+    cell.username.text = name;
+    cell.NumberLbl.text = [NSString stringWithFormat:@"赞数: %@",praise];
+    return cell;
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
--- DayLine/DayLine/SigninViewController.m
+++ DayLine/DayLine/SigninViewController.m
@@ -10,19 +10,112 @@
 #import "TableViewController.h"
 
 @interface SigninViewController (){}
-
+@property (strong, nonatomic) UIActivityIndicatorView *aiv;
 @end
 
 @implementation SigninViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    [[StorageMgr singletonStorageMgr] addKey:@"SignUpSuccessfully" andValue:@NO];
     self.navigationController.navigationBar.hidden = YES;
+}
+
+
+
+//每一次这个页面出现的时候都会调用这个方法，并且时机点是页面将要出现之前
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    //判断是否记忆了用户名
+    if (![[Utilities getUserDefaults:@"Username"] isKindOfClass:[NSNull class]]) {
+        //如果有记忆就把记忆显示在用户名文本输入框中
+        _usernameTF.text = [Utilities getUserDefaults:@"Username"];
+    }
+}
+
+
+
+//每一次这个页面出现的时候都会调用这个方法，并且时机点是页面已然出现以后
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    //判断是否是从注册页面注册成功后回到的这个登录页面
+    if ([[[StorageMgr singletonStorageMgr] objectForKey:@"SignUpSuccessfully"] boolValue]) {
+        //在自动登录前将flag恢复为默认的NO
+        [[StorageMgr singletonStorageMgr] removeObjectForKey:@"SignUpSuccessfully"];
+        [[StorageMgr singletonStorageMgr] addKey:@"SignUpSuccessfully" andValue:@NO];
+        //从单例化全局变量中提取用户名和密码
+        NSString *username = [[StorageMgr singletonStorageMgr] objectForKey:@"Username"];
+        NSString *password = [[StorageMgr singletonStorageMgr] objectForKey:@"Password"];
+        //清除用完的用户名和密码
+        [[StorageMgr singletonStorageMgr] removeObjectForKey:@"Username"];
+        [[StorageMgr singletonStorageMgr] removeObjectForKey:@"Password"];
+        //执行自动登录
+        [self signInWithUsername:username andPassword:password];
+    }
+}
+
+
+
+- (void)popUpShopping {
+    //根据故事版的名称和故事版中页面的名称获得这个页面
+    TableViewController *tabVC = [Utilities getStoryboardInstanceByIdentity:@"Main" byIdentity:@"Tab"];
+    //modal方式跳转到上述页面
+    [self presentViewController:tabVC animated:YES completion:nil];
+    //使用push方法 跳入TabBar [self.navigationController pushViewController:tabVC animated:YES];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+//封装登录操作
+- (void)signInWithUsername:(NSString *)username andPassword:(NSString *)password {
+    //开始登录
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser *user, NSError *error) {
+        //登录完成的回调
+        [_aiv stopAnimating];
+        //判断登录是否成功
+        if (user) {
+            NSLog(@"登录成功");
+            //记忆用户名
+            [Utilities setUserDefaults:@"Username" content:username];
+            //将密码文本输入框中的内容清掉
+            _passwordTF.text = @"";
+            //跳转到首页
+            [self popUpShopping];
+        } else {
+            switch (error.code) {
+                case 101:
+                    [Utilities popUpAlertViewWithMsg:@"用户名或密码错误" andTitle:nil onView:self];
+                    break;
+                case 100:
+                    [Utilities popUpAlertViewWithMsg:@"网络不给力，请稍候再试" andTitle:nil onView:self];
+                    break;
+                default:
+                    [Utilities popUpAlertViewWithMsg:@"服务器正在维护，请稍候再试" andTitle:nil onView:self];
+                    break;
+            }
+        }
+    }];
+}
+
+
+
+
+- (IBAction)signInAction:(UIButton *)sender forEvent:(UIEvent *)event {
+    if (_usernameTF.text.length == 0 || _passwordTF.text.length == 0) {
+        [Utilities popUpAlertViewWithMsg:@"请填写所有信息" andTitle:nil onView:self];
+        return;
+    }
     
     NSDictionary *dic = @{ @"deviceType" : @7001,
                            @"deviceId" : [Utilities uniqueVendor]};
     
+    _aiv = [Utilities getCoverOnView:self.view];
     [RequestAPI getURL:@"/login/getKey" withParameters:dic success:^(id responseObject) {
         NSLog(@"responseOject:%@",responseObject);
         if ([responseObject[@"resultFlag"] integerValue] == 8001) {
@@ -35,47 +128,53 @@
             //将指数与模数存进单例化全局变量
             [[StorageMgr singletonStorageMgr] addKey:@"exponent" andValue:exponent];
             [[StorageMgr singletonStorageMgr] addKey:@"modulus" andValue:modulus];
+            
+            //去单例化全局变量中拿指数与模数
+            //            NSString *exponent = [[StorageMgr singletonStorageMgr] objectForKey:@"exponent"];
+            //            NSString *modulus = [[StorageMgr singletonStorageMgr] objectForKey:@"modulus"];
+            //将原始密码进行MD5加密
+            NSString *md5Pwd = [_passwordTF.text getMD5_32BitString];
+            //将MD5加密过后的密码进行RSA非对称加密
+            NSString *rsaPwd = [NSString encryptWithPublicKeyFromModulusAndExponent:md5Pwd.UTF8String modulus:modulus exponent:exponent];
+            
+            NSDictionary *parameters = @{@"userName" :_usernameTF.text,
+                                         @"password" :rsaPwd,
+                                         @"deviceType" : @7001,
+                                         @"deviceId" : [Utilities uniqueVendor]};
+            
+            [RequestAPI postURL:@"/login" withParameters:parameters success:^(id responseObject)
+             {
+                 NSLog(@"responseOject:%@",responseObject);
+                 if ([responseObject[@"resultFlag"] integerValue] == 8001) {
+                     //执行登录
+                     [self signInWithUsername:_usernameTF.text andPassword:_passwordTF.text];
+                     //[self popUpMall];
+                 }else{
+                     [_aiv stopAnimating];
+                     [Utilities popUpAlertViewWithMsg:@"用户名或密码错误" andTitle:nil onView:self];
+                 }
+                 
+             } failure:^(NSError *error) {
+                 NSLog(@"error:%@",error.description);
+             }];
+        } else {
+            [_aiv stopAnimating];
         }
-        
     } failure:^(NSError *error) {
         NSLog(@"error:%@",error.description);
     }];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
 }
 
-- (IBAction)signInAction:(UIButton *)sender forEvent:(UIEvent *)event {
-    //去单例化全局变量中拿指数与模数
-    NSString *exponent = [[StorageMgr singletonStorageMgr] objectForKey:@"exponent"];
-    NSString *modulus = [[StorageMgr singletonStorageMgr] objectForKey:@"modulus"];
-    //将原始密码进行MD5加密
-    NSString *md5Pwd = [_passwordTF.text getMD5_32BitString];
-    //将MD5加密过后的密码进行RSA非对称加密
-    NSString *rsaPwd = [NSString encryptWithPublicKeyFromModulusAndExponent:md5Pwd.UTF8String modulus:modulus exponent:exponent];
-    NSDictionary *parameters = @{@"userName" :_usernameTF.text,
-                                 @"password" :rsaPwd,
-                                 @"deviceType" : @7001,
-                                 @"deviceId" : [Utilities uniqueVendor]};
-    
-    
-    [RequestAPI postURL:@"/login" withParameters:parameters success:^(id responseObject)
-     {
-         NSLog(@"responseOject:%@",responseObject);
-         if ([responseObject[@"resultFlag"] integerValue] == 8001) {
-             TableViewController *tab = [Utilities getStoryboardInstanceByIdentity:@"Main" byIdentity:@"Tab"];
-             [self presentViewController:tab animated:YES completion:nil];
-             
-         }else{
-             [Utilities popUpAlertViewWithMsg:@"用户名或密码错误" andTitle:nil onView:self];
-         }
-         
-     } failure:^(NSError *error) {
-         NSLog(@"error:%@",error.description);
-     }];
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self.view endEditing:YES];
 }
+
+
 
 - (IBAction)signUpAction:(UIButton *)sender forEvent:(UIEvent *)event {
 }
