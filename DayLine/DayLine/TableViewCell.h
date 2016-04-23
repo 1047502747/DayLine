a08d5916dc02f569b36d58d1cb9593611be45bfe
1047502747 <1047502747@qq.com>
2016-04-23 14:23:22 +0800

    商城页面的实现

--- DayLine/DayLine/TableViewCell.h
+++ DayLine/DayLine/TableViewCell.h
@@ -8,10 +8,27 @@
 
 #import <UIKit/UIKit.h>
 
+//甲方决定何时做，乙方决定怎么做
+//甲方草拟一份协议
+@protocol ActivityTableViewCellDelegate <NSObject>
+
+
+@required
+
+- (void)applyAction: (NSIndexPath *)indexPath;
+- (void)cellLongPressAtIndexPath:(NSIndexPath *)indexPath;
+- (void)photoTapAtIndexPath:(NSIndexPath *)indexPath;
+
+@end
 @interface TableViewCell : UITableViewCell
+@property (strong,nonatomic) NSIndexPath* indexPath;
 @property (weak, nonatomic) IBOutlet UILabel *SPname;
 @property (weak, nonatomic) IBOutlet UILabel *commodityID;
 @property (weak, nonatomic) IBOutlet UILabel *surplus;
 @property (weak, nonatomic) IBOutlet UILabel *integralLbl;
+@property (weak, nonatomic) IBOutlet UIImageView *aviimageView;
 
+
+//甲方打印一份协议
+@property (weak,nonatomic) id <UITableViewDelegate> delegate;
 @end
