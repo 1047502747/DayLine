7074abadb25d8bb2a18d04e9b34e1fb2d2ccc5dd
admin2 <630509237@qq.com>
2016-04-26 15:29:04 +0800

    上传功能

--- DayLine/DayLine/PublishViewController.m
+++ DayLine/DayLine/PublishViewController.m
@@ -8,7 +8,11 @@
 
 #import "PublishViewController.h"
 
-@interface PublishViewController ()
+@interface PublishViewController ()<UIImagePickerControllerDelegate,
+UINavigationControllerDelegate>{
+    BOOL photo;
+}
+@property (strong, nonatomic) UIImagePickerController *imagePC;
 
 @end
 
@@ -34,11 +38,98 @@
 }
 */
 
-- (IBAction)upload:(UIButton *)sender forEvent:(UIEvent *)event {
+
+
+- (void)pickImage:(UIImagePickerControllerSourceType)sourceType {
+    NSLog(@"照片被按了");
+//判断当前选择的图片选择器控制器类型是否可用
+    if ([UIImagePickerController isSourceTypeAvailable:sourceType]) {
+//神奇的nil
+         _imagePC = nil;
+//初始化一个图片选择器控制器对象
+         _imagePC = [[UIImagePickerController alloc] init];
+//签协议
+         _imagePC.delegate = self;
+//设置图片选择器控制器类型
+        _imagePC.sourceType = sourceType;
+//设置选中的媒体文件是否可以被编辑
+        _imagePC.allowsEditing = YES;
+//设置可以被选择的媒体文件的类型
+        _imagePC.mediaTypes = @[(NSString *)kUTTypeImage];
+        [self presentViewController:_imagePC animated:YES completion:nil];
+    } else {
+        UIAlertController *alertView = [UIAlertController alertControllerWithTitle:@"提示" message:sourceType == UIImagePickerControllerSourceTypeCamera ? @"您当前的设备没有照相功能" : @"您当前的设备无法打开相册" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:nil];
+        [alertView addAction:confirmAction];
+        [self presentViewController:alertView animated:YES completion:nil];
+    }
 }
+
+- (IBAction)upload:(UIButton *)sender forEvent:(UIEvent *)event {
+    
+    UIImage *image = _photo.image;
+    NSString *topic = _content.text;
+    
+    if (topic.length == 0 && image == nil) {
+        
+        [Utilities popUpAlertViewWithMsg:@"请填写所有信息或图片" andTitle:nil onView:self];
+        return;
+    }
+    
+    
+    PFObject *posts = [PFObject objectWithClassName:@"Posts"];
+    posts[@"topic"] = topic;
+    PFUser *currentUser = [PFUser currentUser];
+    posts[@"poster"] = currentUser;
+    //设置图片上传
+    if (photo) {
+        UIImage *image = _photo.image;
+        //将图片转换成png格式
+        NSData *imageData = UIImagePNGRepresentation(image);
+        PFFile *imageFile = [PFFile fileWithName:@"1.png" data:imageData];
+        posts[@"photo"] = imageFile;
+    }
+    
+    self.navigationController.view.userInteractionEnabled = NO;
+    UIActivityIndicatorView *aiv = [Utilities getCoverOnView:self.view];
+    [posts saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        self.navigationController.view.userInteractionEnabled = YES;
+        [aiv stopAnimating];
+        if (succeeded) {
+            //创建刷新“我的”页面的通知
+            NSNotification *note = [NSNotification notificationWithName:@"RefreshPost" object:nil];
+            //结合线程触发上述通知（让通知要完成的事先执行完以后再执行触发通知这一行代码后面的代码）
+            [[NSNotificationCenter defaultCenter] performSelectorOnMainThread:@selector(postNotification:) withObject:note waitUntilDone:YES];
+            [self.navigationController popViewControllerAnimated:YES];
+        } else {
+            [Utilities popUpAlertViewWithMsg:@"当前上传用户有点多哦，请稍候再试" andTitle:nil onView:self];
+        }
+    }];
+}
+
+
 
 
 
 - (IBAction)pickAction:(UITapGestureRecognizer *)sender {
+    
+  
 }
+
+
+
+//当选择完媒体文件后调用
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    //根据UIImagePickerControllerEditedImage这个键去拿到我们选中的已经编辑过的图片
+    UIImage *image = info[UIImagePickerControllerEditedImage];
+    //将上面拿到的图片设置为图片视图的图片
+    _photo.image = image;
+    if (image) {
+        photo = YES;
+    }
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+
 @end
