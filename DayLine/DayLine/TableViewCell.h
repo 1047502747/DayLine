7a160bc654ff03ecc3f85a706b596a6dc634cc50
1047502747 <1047502747@qq.com>
2016-04-26 11:34:39 +0800

    no message

--- DayLine/DayLine/TableViewCell.h
+++ DayLine/DayLine/TableViewCell.h
@@ -10,7 +10,7 @@
 
 //甲方决定何时做，乙方决定怎么做
 //甲方草拟一份协议
-@protocol ShoppingTableViewCellDelegate <NSObject>
+@protocol ActivityTableViewCellDelegate <NSObject>
 
 
 @required
@@ -33,5 +33,5 @@
 
 
 //甲方打印一份协议
-@property (weak,nonatomic) id <ShoppingTableViewCellDelegate> delegate;
+@property (weak,nonatomic) id <UITableViewDelegate> delegate;
 @end
