fb4c00bb039e18ba3a3198b03f952299890cd6da
zxk <593735241@qq.com>
2016-05-06 15:26:28 +0800

    添加个人中心

---
+++ DayLine/DayLine/LeftTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  LeftTableViewCell.m
+//  DayLine
+//
+//  Created by zxk on 16/5/6.
+//  Copyright © 2016年 TianXingJian. All rights reserved.
+//
+
+#import "LeftTableViewCell.h"
+
+@implementation LeftTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ DayLine/DayLine/commentViewController.h
@@ -0,0 +1,14 @@
+//
+//  commentViewController.h
+//  DayLine
+//
+//  Created by zxk on 16/5/6.
+//  Copyright © 2016年 TianXingJian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface commentViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITableView *tableview;
+
+@end
