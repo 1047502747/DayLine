aae3fea8cbc423c6f46812dda54df257f004d9eb
zxk <593735241@qq.com>
2016-05-10 17:46:34 +0800

    333

---
+++ DayLine/DayLine/DataTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DataTableViewCell.h
+//  DayLine
+//
+//  Created by zxk on 16/5/10.
+//  Copyright © 2016年 TianXingJian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataTableViewCell : UITableViewCell
+
+@end
---
+++ DayLine/DayLine/DataTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  DataTableViewCell.m
+//  DayLine
+//
+//  Created by zxk on 16/5/10.
+//  Copyright © 2016年 TianXingJian. All rights reserved.
+//
+
+#import "DataTableViewCell.h"
+
+@implementation DataTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
